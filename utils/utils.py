48affa738acadef9012464c5ac72ad712a3cb1e9
m-ttaylor <mttaylor@email.wm.edu>
2022-12-22 13:59:29 -0500

    utils and lint config

--- utils/utils.py
+++ utils/utils.py
@@ -1,4 +1,5 @@
 """bar"""
+from collections import deque
 
 
 def ints(line: str) -> list[int]:
@@ -14,3 +15,43 @@ def ints(line: str) -> list[int]:
             values.append(value)
 
     return values
+
+
+def shortestPath(start, target):
+    queue: deque[list] = deque()
+    visited = set()
+
+    queue.append([start])
+
+    if start == target:
+        return 0
+
+    while len(queue) > 0:
+        path = queue.popleft()
+        node = path[-1]
+
+        if node not in visited:
+            visited.add(node)
+            for nbor in valves2[node]:
+                if nbor == target:
+                    return len(path)
+
+                queue.append([*path, nbor])
+
+
+def executeDFS():
+    def dfs(node, visited):
+
+        if node not in visited:
+            visited.add(node)
+
+        for n in nodes:
+            if n not in visited:
+                dfs(n, visited)
+
+    visited = set()
+
+    for node in nodes:
+        if node not in visited:
+            visited.add(node)
+            dfs(node, visited)
